df5d62fd55f4dc4651f8fa3ccb99861c05c6661e
Gian <gianpetriciaaurora@gmail.com>
2019-07-02 15:58:31 +0700

    Done Add, Update, Delete dan List

---
+++ CrudDemo/Scripts/app.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module("CrudDemoApp", ["CrudDemoApp.controllers", "ngRoute"]);
+
+app.config(["$routeProvider", function ($routerProvider) {
+    $routerProvider.
+        when("/",
+        {
+            templateUrl: "/Partials/PlayerList.html",
+            controller: "MainController"
+        }).
+        when("/AddPlayer",
+            {
+                templateUrl: "/Partials/AddPlayer.html",
+                controller: "AddPlayerController"
+        }).
+        when("/EditPlayer/:id",
+            {
+                templateUrl: "/Partials/EditPlayer.html",
+                controller: "EditPlayerController"
+        }).
+
+        otherwise({ redirectTo: "/" });
+}])
